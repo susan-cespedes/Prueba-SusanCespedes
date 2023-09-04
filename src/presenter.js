e5d0ffef40cd243ab15f2961fb3c329bb293af51
Susan Cespedes <susan.cespedes@ucb.edu.bo>
2023-09-03 23:27:37 -0400

    Mostrar dos posts en la lista de publicaciones

--- src/presenter.js
+++ src/presenter.js
@@ -5,6 +5,9 @@ const detalle = document.querySelector("#detalle");
 const boton = document.querySelector("#publicar-button");
 const div = document.querySelector("#resultado-div");
 
+let postnumero1='';
+let postnumero2='';
+
 boton.addEventListener("click", (event) => {
   event.preventDefault();
 
@@ -12,13 +15,21 @@ boton.addEventListener("click", (event) => {
   const detallepost=detalle.value;
   const resultado=validarcampo(titulopost);
   const longitudMensaje=validarlongitud(titulopost);
+  
 
   if(resultado==false || longitudMensaje==false){
     div.innerHTML = `<p>No puedes ingresar un mensaje sin un titulo/ El texto tiene que ser menor a 50 caracteres</p>
   `;
   }
   else{
-    div.innerHTML = `<p>TITULO:${titulopost}</p>
+    if(postnumero1===""){
+      postnumero1=titulopost;
+    }
+    else if(postnumero2===""){
+      postnumero2=titulopost;
+    }
+    div.innerHTML = `<p>TITULO1:${postnumero1}</p>
+    <p>TITULO2:${postnumero2}</p>
     <p>CONTENIDO:${detallepost}</p>
     `;
   }
